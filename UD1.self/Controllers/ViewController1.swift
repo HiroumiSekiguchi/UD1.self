1c406924aec30bd45899e7ac2e44df76a2e9014e
eHime <kinuyamakomaba@gmail.com>
2019-06-30 20:40:37 +0900

    バグりました。UDにitemContentsとitemCheckedsとcountを保存して頑張ろうとしたが完全にバグった。泣

--- UD1.self/Controllers/ViewController1.swift
+++ UD1.self/Controllers/ViewController1.swift
@@ -6,6 +6,19 @@
 //  Copyright © 2019 関口大海. All rights reserved.
 //
 
+/*
+ UDに関する考察
+ ・itemArrayはUDに保存できない
+ ・itemContentとitemCheckedに分ける
+ 取り出して代入より先に保存を行う
+ ・最終的には再びitemArrayに格納する
+ /
+ itemArrayに入れるのは相変わらずItemのインスタンスでいい。UDに保存するのが上の二点。
+ ・itemArrayはUDで保存しない（というかできない）
+ 
+ 
+ */
+
 import UIKit
 
 class ViewController1: UITableViewController {
@@ -16,20 +29,24 @@ class ViewController1: UITableViewController {
     var item = Item()
     
     var itemArray = [Item]()
+//    var itemNums = [Int]()
+    var itemContents = [String]()
+    var itemCheckeds = [Bool]()
+    var count = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 初期値の設定
-        var item1 = Item()
+        let item1 = Item()
         item1.content = "ご飯を食べる"
         itemArray.append(item1)
         
-        var item2 = Item()
+        let item2 = Item()
         item2.content = "朝早く起きる"
         itemArray.append(item2)
         
-        var item3 = Item()
+        let item3 = Item()
         item3.content = "夜は早く寝る！"
         itemArray.append(item3)
         
@@ -38,9 +55,21 @@ class ViewController1: UITableViewController {
 //            itemArray = items
 //        }
         
+        // itemContentsとitemCheckedsの値をUD内から取得
+        if let iC = defaults.object(forKey: "itemContents") as? [String] {
+            itemContents = iC
+        }
+        if let iCC = defaults.object(forKey: "itemCheckeds") as? [Bool] {
+            itemCheckeds = iCC
+        }
+        
     }
     
-    // ☆☆☆以下、追加ボタン（基礎文法）☆☆☆ //
+    
+    
+    
+    //////// ☆☆☆以下、追加ボタン（基礎文法）☆☆☆ ////////
+    
     @IBAction func addButton(_ sender: UIBarButtonItem) {
         
         // アラートの宣言
@@ -58,14 +87,25 @@ class ViewController1: UITableViewController {
         // アクションの宣言
         let action = UIAlertAction(title: "追加", style: .default) { (action) in
             
-            var newItem = Item()
-            newItem.content = textField.text ?? "項目は追加されませんでした"
-            newItem.checked = false // デフォルト
+            // 追加項目の2要素の宣言
+            let itemContent = textField.text ?? "項目は追加されませんでした"
+            let itemChecked = false // デフォルト
             
+            // 追加項目の宣言と値の代入
+            let newItem = Item()
+//            newItem.content = itemContent
+//            newItem.checked = itemChecked
+            
+            // 配列に追加
+            self.itemContents.append(itemContent)
+            self.itemCheckeds.append(itemChecked)
             self.itemArray.append(newItem)
+            self.count += 1
             
-            // UD内に配列を保存９
-            self.defaults.set(self.itemArray, forKey: "test")
+            // UD内に配列を保存
+            self.defaults.set(self.itemContents, forKey: "itemContents")
+            self.defaults.set(self.itemCheckeds, forKey: "itemCheckeds")
+            self.defaults.set(self.count, forKey: "countitemCheckeds")
             
             // テーブルビューのリロード
             self.tableView.reloadData()
@@ -82,7 +122,11 @@ class ViewController1: UITableViewController {
     
     
     
-    // ☆☆☆以下、TableViewの設定☆☆☆ //
+    
+    
+    
+    
+    //////// ☆☆☆以下、TableViewの設定☆☆☆ //////////
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -95,19 +139,17 @@ class ViewController1: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-        cell.textLabel?.text = itemArray[indexPath.row].content
         
         let itemy = itemArray[indexPath.row]
         
-        // itemのcheckedの真偽によってチェックマークを着け外す
-//        if item.checked == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
-        // 上記をリファクタリング
+        // itemArrayのそのセル番目のitemに二つの値を取ってきて代入
+        itemy.content = itemContents[indexPath.row]
+        itemy.checked = itemCheckeds[indexPath.row]
+
+        cell.textLabel?.text = itemy.content
         
+        // itemのcheckedの真偽によってチェックマークを付け外す
+
         cell.accessoryType = itemy.checked ? .checkmark : .none
         
         return cell
@@ -118,15 +160,7 @@ class ViewController1: UITableViewController {
         // タップしたセルを非選択に戻す
         tableView.deselectRow(at: indexPath, animated: true)
         
-//        // タップしたセルのチェックを付けたり外したりする
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-//        // なぜかリファクタリングできない↑...
-        
-        // itemのcheckedの真偽によってチェックマークを着け外す
+        // タップしたセルのitemの「checked」プロパティを付け替える
         itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
         
         tableView.reloadData()
