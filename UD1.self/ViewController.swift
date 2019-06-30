d4125bac8617d68a5ec60fad1a6758da40fc0e76
eHime <kinuyamakomaba@gmail.com>
2019-06-30 16:42:42 +0900

    Initial Commit

---
+++ UD1.self/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UD1.self
+//
+//  Created by 関口大海 on 2019/06/30.
+//  Copyright © 2019 関口大海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
